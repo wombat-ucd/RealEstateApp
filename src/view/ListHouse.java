582cd55c516573460ab513593f5f8a6f1aa58dbb
Dineesha <dinsuraw@gmail.com>
2015-03-11 09:58:52 +0530

    created realEstate classes

---
+++ src/view/ListHouse.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author User
+ */
+public class ListHouse implements Listable{
+//    private String lastName;
+//    private String firstName;
+//    private int lotNumber;
+//    private int price;
+//    private int squareFeet;
+//    private int bedRooms;
+//    
+//    
+    public  int compareTo(Listable other){
+        return 0;
+        
+    }
+    @Override
+    public Listable copy(){
+        return null;
+    
+}
+//    public String lastName(){
+//        return null;
+//    }
+//    public String firstName(){
+//        return null;
+//    }
+//    public int lotNumber(){
+//        return 0;
+//    }
+//    public int price(){
+//        return 0;
+//    }
+//    public int squareFeet(){
+//        return 0;
+//    }
+//    public int bedRooms(){
+//        return 0;
+//    }
+//    
+//    
+    public Listable[] list;
+    public int size;
+    public int position;
+    
+  
+  
+}
---
+++ src/view/Listable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author User
+ */
+public interface Listable {
+    
+    public abstract int compareTo(Listable other);
+    public abstract Listable copy();
+    
+}
